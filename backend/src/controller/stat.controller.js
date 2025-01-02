32f3058a7cbe2605092417b0d43d0e03f5b90084
SushanthB1 <165074219+SushanthB1@users.noreply.github.com>
2025-01-02 05:35:16 +0530

    Update typo in stat.controller.js

--- backend/src/controller/stat.controller.js
+++ backend/src/controller/stat.controller.js
@@ -1,34 +1,39 @@
-
-import { Song } from "../models/song.model.js";
 import { Album } from "../models/album.model.js";
+import { Song } from "../models/song.model.js";
 import { User } from "../models/user.model.js";
 
-
 export const getStats = async (req, res, next) => {
-    try {
-        const [totalSongs, totalAlbums, totalUsers, uniqueArtists] = await Promise.all([
-            Song.countDocuments(),
-            Album.countDocuments(),
-            User.countDocuments(),
-            Song.aggregate([
-                {
-        $unionWith: {
-            coll: "album",
-            pipeline: []
-        }
-    },
-    {
-        $group: {
-            _id: "$artist", // Group by artist
-        }
-    },
-    {
-        $count: "count" // Count the unique groups
-    }
-            ])
-        ]);
-        res.status(200).json({ totalSongs, totalAlbums, totalUsers, totalArtist: uniqueArtists[0]?.count || 0 });
-    } catch (error) {
-        next(error);
-    }
+	try {
+		const [totalSongs, totalAlbums, totalUsers, uniqueArtists] = await Promise.all([
+			Song.countDocuments(),
+			Album.countDocuments(),
+			User.countDocuments(),
+
+			Song.aggregate([
+				{
+					$unionWith: {
+						coll: "albums",
+						pipeline: [],
+					},
+				},
+				{
+					$group: {
+						_id: "$artist",
+					},
+				},
+				{
+					$count: "count",
+				},
+			]),
+		]);
+
+		res.status(200).json({
+			totalAlbums,
+			totalSongs,
+			totalUsers,
+			totalArtists: uniqueArtists[0]?.count || 0,
+		});
+	} catch (error) {
+		next(error);
+	}
 };
